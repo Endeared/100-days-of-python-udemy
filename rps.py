e9aa7589761592b6b6a41fd061b00374924a8a40
Ross <rosshamey@gmail.com>
2023-01-29 13:19:35 -0500

    Update rps.py

--- rps.py
+++ rps.py
@@ -31,10 +31,10 @@ scissors = '''
 
 options = ['rock', 'paper', 'scissors']
 
-choice = input("What do you choose? Type 0 for ROCK, 1 for PAPER, or 2 for SCISSORS.")
+choice = input("What do you choose? Type 'rock', 'paper', or 'scissors'.\n")
 computerChoice = random.choice(options)
 
-print(f'You have chosen {choice}.')
+print(f'You have chosen {choice.upper()}.')
 if choice.lower() == 'rock':
     print(rock)
 elif choice.lower() == 'paper':
@@ -55,20 +55,19 @@ elif computerChoice.lower() == 'paper':
 elif computerChoice.lower() == 'scissors':
     print(scissors)
 
-
 if choice.lower() == computerChoice:
     print(pyfiglet.figlet_format('TIE!'))
-elif choice.lower() == 'rock' and computerChoice != 'scissors':
-    print(pyfiglet.figlet_format('PLAYER WINS!'))
-elif choice.lower() == 'paper' and computerChoice != 'rock':
-    print(pyfiglet.figlet_format('PLAYER WINS!'))
-elif choice.lower() == 'scissors' and computerChoice != 'paper':
-    print(pyfiglet.figlet_format('PLAYER WINS!'))
 elif choice.lower() == 'rock' and computerChoice != 'paper':
-    print(pyfiglet.figlet_format('COMPUTER WINS!'))
+    print(pyfiglet.figlet_format('PLAYER WINS!'))
 elif choice.lower() == 'paper' and computerChoice != 'scissors':
-    print(pyfiglet.figlet_format('COMPUTER WINS!'))
+    print(pyfiglet.figlet_format('PLAYER WINS!'))
 elif choice.lower() == 'scissors' and computerChoice != 'rock':
+    print(pyfiglet.figlet_format('PLAYER WINS!'))
+elif choice.lower() == 'rock' and computerChoice == 'paper':
+    print(pyfiglet.figlet_format('COMPUTER WINS!'))
+elif choice.lower() == 'paper' and computerChoice == 'scissors':
+    print(pyfiglet.figlet_format('COMPUTER WINS!'))
+elif choice.lower() == 'scissors' and computerChoice == 'rock':
     print(pyfiglet.figlet_format('COMPUTER WINS!'))
 exit()
 
