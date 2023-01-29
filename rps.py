4342bfa7cf4a04d170c4c9d21c1a519c96ff20f7
Ross <rosshamey@gmail.com>
2023-01-29 12:58:57 -0500

    Update rps.py

--- rps.py
+++ rps.py
@@ -32,3 +32,13 @@ options = ['rock', 'paper', 'scissors']
 choice = input("What do you choose? Type 0 for ROCK, 1 for PAPER, or 2 for SCISSORS.")
 computerChoice = random.choice(options)
 
+print(f'You have chosen {choice}.')
+if choice.lower() == 'rock':
+    print(rock)
+elif choice.lower() == 'paper':
+    print(paper)
+elif choice.lower() == 'scissors':
+    print(scissors)
+else:
+    print('Your input is invalid. Please restart the game.')
+    exit()
