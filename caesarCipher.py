9b36ec7b04aa6b249ac965b68e841d44b489c943
Ross <rosshamey@gmail.com>
2023-02-01 15:08:04 -0500

    Update caesarCipher.py

--- caesarCipher.py
+++ caesarCipher.py
@@ -8,7 +8,7 @@ while True:
 
     option = input(f"Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     message = input(f"Type your message:\n").lower()
-    shift = str(input(f"Type the shift number:\n"))
+    shift = input(f"Type the shift number:\n")
     newMessage = ""
 
     if option == 'encode':
@@ -16,4 +16,15 @@ while True:
             if char not in alphabet:
                 newMessage += char
             else:
+                charIndex = alphabet.index(char)
+                newIndex = charIndex + shift
+                if newIndex > 25:
+                    newIndex = newIndex % 26
+                newMessage += alphabet[newIndex]
+        print(f'Your encoded message is: {newMessage}')
+    elif option == 'decode':
+        for char in message:
+            if char not in alphabet:
+                newMessage += char
+
 
