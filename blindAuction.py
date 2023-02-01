37de3e5aee4118f8ac8733956cfae2b001323606
Ross <114155576+Endeared@users.noreply.github.com>
2023-02-01 18:30:48 -0500

    Create blindAuction.py

---
+++ blindAuction.py
@@ -0,0 +1,12 @@
+from dependencies.auctionArt import logo
+import os
+
+clear = lambda: os.system('cls')
+addingPlayers = True
+
+while True:
+
+    print(logo)
+
+    while addingPlayers == True:
+        
