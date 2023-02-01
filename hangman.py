b2f647e5ed99e01f41ff92c2d7f5253fc401405b
Ross <rosshamey@gmail.com>
2023-02-01 13:02:27 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,20 @@
 import random
 from dependencies.hangman_art import logo
 from dependencies.hangman_words import word_list
+
+
+thisWord = random.choice(word_list)
+thisLength = len(thisWord)
+lives = 6
+
+wordGuessed = []
+
+print(logo)
+
+for blank in range(thisLength):
+    wordGuessed += "_"
+
+while True:
+    guess = input("Please gues a letter: ").lower()
+
+    if guess in wordGuessed
