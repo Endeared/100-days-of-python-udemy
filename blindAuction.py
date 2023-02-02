a31a0c54845360bc0e4de5083a2b7443e0210eed
Ross <114155576+Endeared@users.noreply.github.com>
2023-02-01 19:09:44 -0500

    Update blindAuction.py

--- blindAuction.py
+++ blindAuction.py
@@ -6,7 +6,9 @@ addingPlayers = True
 
 while True:
 
+    playerDict = {}
     print(logo)
 
     while addingPlayers == True:
-        
+        name = input(f'What is your name? ').lower()
+        bid = int(input(f'What is your bind? $'))
