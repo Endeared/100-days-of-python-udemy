8098c2e8a55e8660d8b596789d72ce65f163318e
Ross <114155576+Endeared@users.noreply.github.com>
2023-02-01 19:32:07 -0500

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -7,3 +7,29 @@ while True:
     firstNum = float(input(f'What is the first number?\n'))
     operation = input('Choose an operation.\n+\n-\n*\n/\n')
     secondNum = float(input(f'What is the second number?\n'))
+
+    if operation == '+':
+        sum = firstNum + secondNum
+        print(f'{firstNum} + {secondNum} = {sum}')
+        firstNum = sum
+        secondNum = None
+    elif operation == '-':
+        sum = firstNum - secondNum
+        print(f'{firstNum} - {secondNum} = {sum}')
+        firstNum = sum
+        secondNum = None
+    elif operation == '*':
+        product = firstNum * secondNum
+        print(f'{firstNum} * {secondNum} = {product}')
+        firstNum = product
+        secondNum = None
+    elif operation == '/':
+        divisor = firstNum / secondNum
+        print(f'{firstNum} / {secondNum} = {divisor}')
+        firstNum = divisor
+        secondNum = None
+    else:
+        print('Invalid operation / number(s). Please retry.')
+        continue
+
+    check = input(f"Type 'y' to continue calculating with {firstNum}, or type 'n' to start with a new number.")
